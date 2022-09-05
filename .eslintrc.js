050f111f77f18ee463d2dbd04ec7119cd14c289c
wangyang <wangyang.0210@foxmail.com>
2022-09-05 22:51:10 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        'standard-with-typescript',
+        'plugin:prettier/recommended'
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['tsconfig.jsong']
+    },
+    plugins: ['vue'],
+    ignorePatterns: ['node_modules/**', 'dist/**', 'release/**', 'public/**', 'docs/**'],
+    rules: {}
+}
---
+++ docs/changelog.md
@@ -0,0 +1,6 @@
+## 2022.09.04 - int
+
+-   项目创建
+-   依赖升级
+-   tsconfig 开启`forceConsistentCasingInFileNames`和`strict`
+-   集成`eslint`,`prettier`,`husky`
