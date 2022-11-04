912962884964ccf93ac12af850faf09a990b6c70
wangyang <wangyang.0210@foxmail.com>
2022-11-04 10:49:31 +0800

    chore: cherry-markdown

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,17 @@
 <template>
-    <p>
-        Recommended IDE setup:
-        <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-        +
-        <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    </p>
-    <p>
-        <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
-        |
-        <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-    </p>
-
-    <button type="button" @click="count++">count is: {{ count }}</button>
-    <p>
-        Edit
-        <code>components/HelloWorld.vue</code> to test hot module replacement.
-    </p>
+    <div v-html="htmlContent"></div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
+import CherryEngine from 'cherry-markdown/dist/cherry-markdown.engine.core'
+const cherryEngineInstance = new CherryEngine()
 
-const count = ref(0)
+let htmlContent = ref(0)
+htmlContent = cherryEngineInstance.makeHtml('# welcome to cherry editor!')
 
 onMounted(() => {
-    console.log(`The initial count is ${count.value}.`)
+    console.log()
 })
 </script>
 
